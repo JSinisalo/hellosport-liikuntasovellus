f291dfa45dc4e54656bf10ae2017694645e23ee5
Akio Ide <akio.ide@cs.tamk.fi>
2018-03-23 02:07:48 +0200

    trying to push my change

--- src/main/resources/static/modalForm.js
+++ src/main/resources/static/modalForm.js
@@ -1,4 +1,4 @@
-    // Get the modal
+
     var modal = document.getElementById('myModal');
     // Get the button that opens the modal
     var addButton = document.getElementById('addButton');
@@ -9,7 +9,10 @@
     let title = document.getElementById("title");
     let author = document.getElementById("author");
     let textBody = document.getElementById("textBody");
-   
+    let searchStart = document.getElementById("start_time");
+    let searchEnd = document.getElementById("end_time");
+    let gender = document.getElementById("gender");
+    let sport = document.getElementById("sport");
     createBlogpostTable(document.body);
     // When the user clicks the button, open the modal 
     addButton.onclick = function() {
@@ -27,9 +30,13 @@
         method: 'POST', 
         body: JSON.stringify({
 
-            title: title.value,
-            textBody: textBody.value,
-            authorName: author.value
+          title: title.value,
+          textBody: textBody.value,
+          authorName: author.value,
+          searchStart: searchStart.value,
+          searchEnd: searchEnd.value,
+          gender: gender.value.split(' '),
+          sport: sport.value.split(' ')
         }),
         headers: new Headers({ 'Content-Type': 'application/json'}) }).then((r) => { console.log(r); window.location.reload(false); }); 
         
@@ -63,30 +70,34 @@
       return element;
   }
   
-  function createBlogpostTable(e) {
-  
-      let tbl  = document.createElement('table');
-  
-      tbl.style.border = '1px solid black';
-  
-      tbl.setAttribute('id','table1');
-  
-      let tr = tbl.insertRow();
-  
-      fetch('http://localhost:8080/notifications').then((response) => response.json()).then((arr) => {
-  
-          for(let j = 0; j < arr.length; j++) {
-  
-              let tr = tbl.insertRow();
-      
-              tr.insertCell().appendChild(document.createTextNode(arr[j].title));
-              tr.insertCell().appendChild(document.createTextNode(arr[j].authorName));
-              tr.insertCell().appendChild(document.createTextNode(arr[j].textBody));
-              let b = tr.insertCell().appendChild(document.createElement('button'));
-              b.innerHTML = 'DELET';
-              b.addEventListener('click',() => { fetch('http://localhost:8080/notifications/' + arr[j].id, { method: 'delete' }).then(() => { window.location.reload(false); }); });
-          }
-      });
+function createBlogpostTable(e) {
+
+    let tbl  = document.createElement('table');
+
+    tbl.style.border = '1px solid black';
+
+    tbl.setAttribute('id','table1');
+
+    let tr = tbl.insertRow();
+
+    fetch('http://localhost:8080/notifications').then((response) => response.json()).then((arr) => {
+
+        for(let j = 0; j < arr.length; j++) {
+
+            let tr = tbl.insertRow();
+    
+            tr.insertCell().appendChild(document.createTextNode(arr[j].title));
+            tr.insertCell().appendChild(document.createTextNode(arr[j].authorName));
+            tr.insertCell().appendChild(document.createTextNode(arr[j].textBody));
+            tr.insertCell().appendChild(document.createTextNode(arr[j].searchStart));
+            tr.insertCell().appendChild(document.createTextNode(arr[j].searchEnd));
+            tr.insertCell().appendChild(document.createTextNode(arr[j].gender));
+            tr.insertCell().appendChild(document.createTextNode(arr[j].sport));
+            let b = tr.insertCell().appendChild(document.createElement('button'));
+            b.innerHTML = 'DELET';
+            b.addEventListener('click',() => { fetch('http://localhost:8080/notifications/' + arr[j].id, { method: 'delete' }).then(() => { window.location.reload(false); }); });
+        }
+    });
   
       e.appendChild(tbl);
   }
