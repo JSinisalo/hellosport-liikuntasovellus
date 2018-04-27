a40c6c6c4db1adacb9c8ea5455c273d6226be68f
TommiSpalding <thomas.spalding@cs.tamk.fi>
2018-04-27 20:55:50 +0300

    Fixed frontend building problems. Added one on spring.

--- frontend/liikunta/src/index.js
+++ frontend/liikunta/src/index.js
@@ -6,4 +6,5 @@ import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+console.log('perse');
 registerServiceWorker();
