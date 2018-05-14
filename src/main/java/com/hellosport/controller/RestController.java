b1c4e52c964c4f9760efac8c1d8aed508542dd43
TommiSpalding <thomas.spalding@cs.tamk.fi>
2018-05-14 12:26:35 +0300

    Modified the security controller to answer if admin.

--- src/main/java/com/hellosport/controller/RestController.java
+++ src/main/java/com/hellosport/controller/RestController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -78,6 +80,7 @@ public class RestController {
     public ResponseEntity<Boolean> checkAdminNotification() {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
 
         if(auth.getPrincipal().equals("admin"))
             return new ResponseEntity<Boolean>(true, HttpStatus.OK);
--- src/main/java/com/hellosport/controller/SecurityController.java
+++ src/main/java/com/hellosport/controller/SecurityController.java
@@ -1,6 +1,7 @@
 package com.hellosport.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,14 @@ public class SecurityController {
     @RequestMapping(value = "/username", method = RequestMethod.GET)
     @ResponseBody
     public String currentUserName(Authentication authentication) {
-        return authentication.getPrincipal().toString();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String output = "";
+        if (userDetails.getAuthorities().toString().contains("ROLE_ADMIN")) {
+            output = "yes";
+        } else {
+            output = "no";
+        }
+        return output;
+
     }
 }
